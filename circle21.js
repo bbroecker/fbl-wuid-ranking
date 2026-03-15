15495c9c023aa6a4fe52a93a8ce0d53cd4edd11e
Bastian Broecker <bastian.broecker@gmail.com>
2026-03-15 19:14:19 +0100

    team breakdown

--- circle21.js
+++ circle21.js
@@ -1145,7 +1145,7 @@ function showToast(message) {
 }
 
 // ====================================
-// TEAM BREAKDOWN DISPLAY
+// TEAM POINTS BREAKDOWN DISPLAY
 // ====================================
 
 function populateTeamBreakdownSelect() {
